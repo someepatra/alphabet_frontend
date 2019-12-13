0b836f32877e70ac64ccec088a39caa44fcb8478
Mausumi Patra <someepatra@gmail.com>
2019-12-13 15:27:19 -0500

    updated quiz component

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -1,36 +1,148 @@
 import React, { Component } from "react";
+import { Redirect } from 'react-router-dom'
+import axios from "axios"
+
 class Quiz extends Component {
     constructor() {
         super();
         this.state = {
             cards: [],
             alphabets: [],
+            index: 1,
             displayCard: {},
-            onealpha: {}
+            onealpha: {},
+            toHomePage: false,
+            random1: {},
+            random2: {}
+        }
+        this.nextQuiz = this.nextQuiz.bind(this)
+        this.callAPI = this.callAPI.bind(this)
+        this.renderRedirect = this.renderRedirect.bind(this)
+        this.shuffleImages = this.shuffleImages.bind(this)
+        this.shuffle = this.shuffle.bind(this)
+    }
+
+    async callAPI() {
+        try {
+            const request = await axios.get("/alphabets");
+            console.log("show", request);
+            const alphabetData = request.data;
+            this.setState({
+                alphabets: alphabetData,
+            });
+        } catch (err) {
+            console.log("show error: ", err);
         }
     }
-    async componentDidMount() {
-        await this.setState({
-            alphabets: this.props.alphabets,
-            onealpha: this.props.alphabets[0],
-            displayCard: this.props.alphabets[0].cards[0]
-        })
-        console.log(this.props.alphabets)
 
+    async componentDidMount() {
+
+        await this.callAPI();
+
+        let cardList = this.state.alphabets.map(alphabet => {
+            return alphabet.cards[0]
+        })
+
+        this.setState({
+            cards: cardList
+        })
+
+        this.setState({
+            onealpha: this.state.alphabets[0],
+            displayCard: this.state.cards[0]
+        })
+
+        let randomIndex1 = 0;
+        let randomIndex2 = 0;
+
+
+        do {
+            randomIndex1 = Math.floor(Math.random() *
+                this.state.alphabets.length);
+            randomIndex2 = Math.floor(Math.random() *
+                this.state.alphabets.length);
+        } while (randomIndex1 === randomIndex2 || randomIndex1 === this.state.index || randomIndex2 === this.state.index)
+
+        this.setState({
+            random1: this.state.alphabets[randomIndex1].cards[0],
+            random2: this.state.alphabets[randomIndex2].cards[0]
+        })
 
     }
+
+    nextQuiz() {
+
+        if (this.state.index < 26) {
+            this.setState({
+                index: this.state.index + 1
+            })
+
+
+            let randomIndex1 = 0;
+            let randomIndex2 = 0;
+
+
+            do {
+                randomIndex1 = Math.floor(Math.random() *
+                    this.state.alphabets.length);
+                randomIndex2 = Math.floor(Math.random() *
+                    this.state.alphabets.length);
+            } while (randomIndex1 === randomIndex2 || randomIndex1 === this.state.index || randomIndex2 === this.state.index)
+
+            this.setState({
+                onealpha: this.state.alphabets[this.state.index],
+                displayCard: this.state.cards[this.state.index],
+                random1: this.state.alphabets[randomIndex1].cards[0],
+                random2: this.state.alphabets[randomIndex2].cards[0]
+            })
+        } else {
+            this.setState({
+                toHomePage: true
+            })
+        }
+    }
+
+    renderRedirect = () => {
+        if (this.state.toHomePage) {
+            return <Redirect to='/' />
+        }
+    }
+
+    shuffle(array) {
+        array.sort(() => Math.random() - 0.5);
+    }
+
+    shuffleImages = () => {
+
+        let images = [
+            (<img onClick={() => this.nextQuiz()} src={this.state.displayCard.img} alt={this.state.displayCard.name} />),
+            (<img src={this.state.random1.img} alt={this.state.displayCard.name} />),
+            (<img src={this.state.random2.img} alt={this.state.displayCard.name} />)
+        ]
+
+        this.shuffle(images);
+
+        return (
+            <>
+                {images[0]}
+                {images[1]}
+                {images[2]}
+            </>
+        )
+    }
+
     render() {
         return (
             <div className="mainDiv">
+                {this.renderRedirect()}
                 <div className="leftSide">
                     <div className="quizAlpha">
                         {this.state.onealpha.alp_name}
                     </div>
                 </div>
 
-                <div className="rightSide">
-                    <img src={this.state.displayCard.img} alt={this.state.displayCard.name} />
-
+                <div className="rightSide" className="card">
+                    {this.shuffleImages()}
                 </div>
             </div>
 
