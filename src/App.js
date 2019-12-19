be78d26b8c37a8364925f78999ebea44bcea3e39
Mausumi Patra <someepatra@gmail.com>
2019-12-19 12:38:25 -0500

    update app.js

--- src/App.js
+++ src/App.js
@@ -5,10 +5,10 @@ import axios from "axios";
 import Alphabets from './components/Alphabets.js'
 import { BrowserRouter as Router, Route } from "react-router-dom"
 import Quiz from "./components/Quiz";
-let baseURL = "https://alphabets-game.herokuapp.com"
+let base_url = "https://alphabets-game.herokuapp.com"
 
 if (process.env.NODE_ENV === 'development') {
-  baseURL = 'http://localhost:3003'
+  base_url = 'http://localhost:3003'
 }
 class App extends Component {
   constructor() {
